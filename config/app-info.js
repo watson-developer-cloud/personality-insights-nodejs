037c97a8940f841c6545c5d9e112244cefff1fc0
Ary Pablo Batista <batarypa@ar.ibm.com>
2016-03-29 14:03:51 -0300

    app-info module

---
+++ config/app-info.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+
+let
+  yml      = require('../utilities/file/require-yaml'),
+  manifest = yml('../manifest.yml');
+
+const
+  ENV = process.env.NODE_ENV,
+  APP_NAME = manifest.applications.name,
+  DOMAIN   = ENV === 'production' ? APP_NAME + '.mybluemix.net'
+                                  : 'uqbar.local',
+
+  PORT = process.env.VCAP_APP_PORT || 3000,
+  PROTOCOL = process.env.SECURE_EXPRESS ? 'https' : 'http',
+  URL  = ENV === 'production' ? `${PROTOCOL}://${DOMAIN}` : `${PROTOCOL}://${DOMAIN}:${PORT}`;
+
+module.exports = {
+  app_name    : APP_NAME,
+  domain      : DOMAIN,
+  environment : ENV,
+  port        : PORT,
+  url         : URL
+};
