6ee4bf8f6053a2c512c7ddac1c04a712ae8e8854
Agustin Casamayor <agustincasamayor@gmail.com>
2016-05-16 20:20:06 -0300

    Added latest build of trait-descriptions component, with Spanish support

--- public/js/components/personality-trait-descriptions.js
+++ public/js/components/personality-trait-descriptions.js
@@ -19,7 +19,7 @@
 
 module.exports = require('./lib/personality-trait-descriptions');
 
-},{"./lib/personality-trait-descriptions":5}],2:[function(require,module,exports){
+},{"./lib/personality-trait-descriptions":6}],2:[function(require,module,exports){
 /**
  * Copyright 2016 IBM Corp. All Rights Reserved.
  *
@@ -39,10 +39,11 @@ module.exports = require('./lib/personality-trait-descriptions');
 'use strict';
 
 module.exports = {
-  'en': require('./en')
+  'en': require('./en'),
+  'es': require('./es')
 };
 
-},{"./en":3}],3:[function(require,module,exports){
+},{"./en":3,"./es":4}],3:[function(require,module,exports){
 'use strict';
 /*
  * Copyright 2015-2016 IBM Corp. All Rights Reserved.
@@ -127,6 +128,90 @@ module.exports = {
 };
 
 },{}],4:[function(require,module,exports){
+'use strict';
+/*
+ * Copyright 2015-2016 IBM Corp. All Rights Reserved.
+#
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+#
+ *      http://www.apache.org/licenses/LICENSE-2.0
+#
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use static';
+
+module.exports = {
+
+  big5: {
+    'Openness': 'Apertura a las experiencias. Más alto: Intelectualmente curiosos, emocionalmente conscientes, sensibles a la belleza, y deseosos por probar cosas nuevas. \nMás bajo: Prefieren lo común, directo, y obvio, frente a lo complicado, ambiguo y sutil.',
+    'Conscientiousness': 'Más alto: Auto-disciplinados, conscientes de sus deberes, o tendientes a querer alcanzar logros por encima de las medidas o expectativas externas. \nMás bajo: Más propensos a preferir lo espontáneo por sobre lo planificado.',
+    'Extraversion': 'Más alto: Más enérgico y marcado compromiso con el mundo externo. Les gusta la alta visibilidad en grupos, hablar y demostrarse a si mismos. \nMás bajo: Necesitan menos estimulación y son más independientes de su mundo social. No significa que sean tímidos, no amistosos, o antisociales.',
+    'Agreeableness': 'Más alto: Valoran llevarse bien con otros. Tienen una visión más optimista de la naturaleza humana. \nMás bajo: Valoran sus propios intereses por sobre los de otros. Son más escépticos de los motivos de los demás.',
+    'Neuroticism': '**Esta demo no puede diagnosticar una enfermedad mental.** Más alto: Más promensos a tener emociones negativas o disgustarse. Puede significar que estén pasando por un mañ momento. \nMás bajo: Más calmos y menos probables a disgustarse o alterarse. Esto no significa que sean gente positiva o feliz.',
+    'Adventurousness': 'Entusiasmo por probar nuevas actividades y experimentar nuevas cosas.',
+    'Artistic interests': 'Apreciación por el arte y la belleza, tanto humana como de la naturaleza.',
+    'Emotionality': 'Disponibilidad emocional; conciencia de los sentimientos propios.',
+    'Imagination': 'Apertira a crear un mundo interior de fantasía.',
+    'Intellect': 'Curiosidad intelectual; apertura a nuevas ideas.',
+    'Liberalism': 'Apertura a re-examinar los valores propios y tradiciones; disposición para desafiar la autoridad.',
+    'Achievement striving': 'Necesidad de realizar logros personales y sentido de la dirección.',
+    'Cautiousness': 'Tendencia a pensar las cosas antes de actuar o hablar.',
+    'Dutifulness': 'Sentido del deber; gran énfasis puesto en cumplir con las obligaciones.',
+    'Orderliness': 'Organización personal; prolijidad y pulcritud.',
+    'Self-discipline': 'Fuerza de voluntad; la capacidad de empezar tareas y seguirlas hasta el final, a pesar del aburrimiento o distracciones.',
+    'Self-efficacy': 'Creencia en la capacidad propia.',
+    'Activity level': 'Ritmo de vida; nivel de ocupación.',
+    'Assertiveness': 'Fuerza de expresión; búsqueda del liderazgo y ascendencia social; deseo por dirigir las actividades de otros.',
+    'Cheerfulness': 'Tendencia a experimentar o expresar emociones positivas.',
+    'Excitement-seeking': 'Necesidad por estimulación del entorno.',
+    'Friendliness': 'Interés y amistad hacia otros; confiado socialmente.',
+    'Gregariousness': 'Gusto por la compañía de otros; sociabilidad.',
+    'Altruism': 'Preocupación activa y genuina por el bienestar de los demás.',
+    'Cooperation': 'Disgusto por las confrontaciones; responde a los conflictos interpersonales con el deseo de compromiso.',
+    'Modesty': 'Tendencia a la sencillez, pocas pretensiones, sin alarde de los logros propios; humildad. ',
+    'Morality': 'Franqueza y expresión genuina; cándido, franco.',
+    'Sympathy': 'Actitud de compasión hacia los demás; amabilidad.',
+    'Trust': 'Nivel de creencia en la sinceridad y buenas intenciones de los demás.',
+    'Anger': 'Tendencia a experimentar –pero no necesariamente expresar– ira o frustración.',
+    'Anxiety': 'Tendencia a vivir enfocado en las dificultades y problemas; experimenta inquietud y problemas con facilidad. ',
+    'Depression': 'Tendencia normal a experimental sentimientos de culpa, tristeza, desesperanza, o soledad. **Esta demo no puede diagnosticar una enfermedad mental.**',
+    'Immoderation': 'Tendencia a actuar con ansiedad y urgencia. Tienden a los placeres y recompensas de corto plazo en lugar de consecuencias a largo plazo.',
+    'Self-consciousness': 'Temor al rechazo o vergüenza; timidez.',
+    'Vulnerability': 'Dificultad a lidiar con el estrés o la presión en situaciones complicadas.'
+  },
+
+  needs: {
+    'Structure': 'Necesidad de organización, planificación, y cosas que tengan un propósito claro.',
+    'Stability': 'Necesidad por lo tangible, probado y experimentado, con un buen prontuario e historial.',
+    'Self-expression': 'Deseo por descubrir y plasmar la identidad propia.',
+    'Practicality': 'Deseo por completar una tarea, con habilidad y eficiencia.',
+    'Love': 'Contacto social, tanto uno-a-uno como uno-a-muchos..',
+    'Liberty': 'Necesidad de escapar, deseo por nuevas experiencias y cosas nuevas.',
+    'Ideal': 'Deseo de satisfacer las ideas propias de perfección en el estilo de vida o experiencias; generalmente visto como perseguir un deseo de comunidad.',
+    'Harmony': 'Necesidad de complacer a otras personas, sus puntos de vista y sentimientos.',
+    'Excitement': 'Necesidad de perseguir experiencias o llevar un estilo de vida que muestre entusiasmo y deseo.',
+    'Curiosity': 'Necesidad de perseguir experiencias que fomenten el aprendizaje, la exploración y el crecimiento.',
+    'Closeness': 'Necesidad de cuidar y ser cuidado; sentido de pertenencia.',
+    'Challenge': 'Deseo de alcanzar, lograr, competir, o perseguir experiencias que ponen a prueba las propias habilidades.'
+  },
+
+  values: {
+    'Conservation': 'Respeto, compromiso, y aceptación de las costumbres e ideas que proveen la propia cultura y/o religión.',
+    'Openness to change': 'Independencia en las acciones, los pensamientos, sentimientos y disposición a nuevas experiencias.',
+    'Hedonism': 'Búsqueda del placer y de la gratificación de los sentidos.',
+    'Self-enhancement': 'Éxito personal a través de la demostración de competencia de acuerdo a estándares sociales.',
+    'Self-transcendence': 'Preservación y mejora del bienestar de aquellos con los que se contacta frecuentemente.'
+  }
+};
+
+},{}],5:[function(require,module,exports){
 /**
  * Copyright 2016 IBM Corp. All Rights Reserved.
  *
@@ -180,7 +265,7 @@ var I18nData = function () {
 
 module.exports = I18nData;
 
-},{"./dictionaries":2,"underscore":9}],5:[function(require,module,exports){
+},{"./dictionaries":2,"underscore":10}],6:[function(require,module,exports){
 /**
  * Copyright 2016 IBM Corp. All Rights Reserved.
  *
@@ -236,7 +321,7 @@ module.exports = function () {
   return _class;
 }();
 
-},{"./i18n":4,"./utilities/format-text":6,"underscore":9}],6:[function(require,module,exports){
+},{"./i18n":5,"./utilities/format-text":7,"underscore":10}],7:[function(require,module,exports){
 /**
  * Copyright 2016 IBM Corp. All Rights Reserved.
  *
@@ -277,7 +362,7 @@ function formatText(text, options) {
 
 module.exports = formatText;
 
-},{"marked":7,"remove-markdown":8}],7:[function(require,module,exports){
+},{"marked":8,"remove-markdown":9}],8:[function(require,module,exports){
 (function (global){
 /**
  * marked - a markdown parser
@@ -1566,7 +1651,7 @@ if (typeof module !== 'undefined' && typeof exports === 'object') {
 }());
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],8:[function(require,module,exports){
+},{}],9:[function(require,module,exports){
 module.exports = function(md, options) {
   options = options || {};
   options.stripListLeaders = options.hasOwnProperty('stripListLeaders') ? options.stripListLeaders : true;
@@ -1616,7 +1701,7 @@ module.exports = function(md, options) {
   return output;
 };
 
-},{}],9:[function(require,module,exports){
+},{}],10:[function(require,module,exports){
 //     Underscore.js 1.8.3
 //     http://underscorejs.org
 //     (c) 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
