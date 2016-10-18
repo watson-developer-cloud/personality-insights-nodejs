f28815417571aaf41f7852b68c7eda879c21bb37
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2016-10-18 16:52:59 -0400

    also reorder consumption prefs based on list in #412

--- public/js/demo.js
+++ public/js/demo.js
@@ -380,6 +380,61 @@ $(document).ready(function () {
     });
   }
 
+  function cpIdSorting(cpid) {
+    return ["consumption_preferences_music_rap",
+      "consumption_preferences_music_country",
+      "consumption_preferences_concerned_environment",
+      "consumption_preferences_read_frequency",
+      "consumption_preferences_music_r_b",
+      "consumption_preferences_volunteer_learning",
+      "consumption_preferences_automobile_ownership_cost",
+      "consumption_preferences_automobile_safety",
+      "consumption_preferences_volunteer",
+      "consumption_preferences_movie_romance",
+      "consumption_preferences_eat_out",
+      "consumption_preferences_music_hip_hop",
+      "consumption_preferences_movie_adventure",
+      "consumption_preferences_movie_horror",
+      "consumption_preferences_influence_brand_name",
+      "consumption_preferences_music_live_event",
+      "consumption_preferences_clothes_quality",
+      "consumption_preferences_automobile_resale_value",
+      "consumption_preferences_clothes_style",
+      "consumption_preferences_read_motive_enjoyment",
+      "consumption_preferences_music_christian_gospel",
+      "consumption_preferences_read_motive_information",
+      "consumption_preferences_books_entertainment_magazines",
+      "consumption_preferences_books_non_fiction",
+      "consumption_preferences_start_business",
+      "consumption_preferences_read_motive_mandatory",
+      "consumption_preferences_gym_membership",
+      "consumption_preferences_influence_family_members",
+      "consumption_preferences_adventurous_sports",
+      "consumption_preferences_movie_musical",
+      "consumption_preferences_movie_historical",
+      "consumption_preferences_movie_science_fiction",
+      "consumption_preferences_volunteering_time",
+      "consumption_preferences_spur_of_moment",
+      "consumption_preferences_movie_war",
+      "consumption_preferences_credit_card_payment",
+      "consumption_preferences_movie_drama",
+      "consumption_preferences_read_motive_relaxation",
+      "consumption_preferences_influence_utility",
+      "consumption_preferences_music_playing",
+      "consumption_preferences_books_financial_investing",
+      "consumption_preferences_fast_food_frequency",
+      "consumption_preferences_movie_action",
+      "consumption_preferences_influence_online_ads",
+      "consumption_preferences_books_autobiographies",
+      "consumption_preferences_influence_social_media",
+      "consumption_preferences_music_latin",
+      "consumption_preferences_music_rock",
+      "consumption_preferences_outdoor",
+      "consumption_preferences_music_classical",
+      "consumption_preferences_movie_documentary",
+      "consumption_preferences_clothes_comfort" ].indexOf(cpid);
+  }
+
   function cpIdMapping(cpid) {
     return {
       "consumption_preferences_automobile_ownership_cost": "be sensitive to ownership cost when buying automobiles",
@@ -445,7 +500,8 @@ $(document).ready(function () {
       var likelycps = data.consumption_preferences.reduce(function(k,v) {
         v.consumption_preferences.map(function(child_item) {
           if (child_item.score === 1) {
-            k.push(cpIdMapping(child_item.consumption_preference_id));
+            k.push({ name: cpIdMapping(child_item.consumption_preference_id),
+              idx: cpIdSorting(child_item.consumption_preference_id) });
           }
         });
         return k;
@@ -454,7 +510,8 @@ $(document).ready(function () {
       var unlikelycps = data.consumption_preferences.reduce(function(k,v) {
         v.consumption_preferences.map(function(child_item) {
           if (child_item.score === 0) {
-            k.push(cpIdMapping(child_item.consumption_preference_id));
+            k.push({ name: cpIdMapping(child_item.consumption_preference_id),
+              idx: cpIdSorting(child_item.consumption_preference_id) });
           }
         });
         return k;
@@ -462,14 +519,14 @@ $(document).ready(function () {
       behaviors.html("");
       behaviors.append("<h4 class=\"base--h4\">You are likely to______ </h4>");
       behaviors.append("<div class=\"output-summary--likely-behaviors\">");
-      likelycps.slice(0,3).map(function(item) {
-        behaviors.append("<div class=\"output-summary--behavior output-summary--behavior_POSITIVE\"><i class=\"icon icon-likely\"></i>" + item + "</div>\n");
+      likelycps.sort(function(l,r) { return l.idx > r.idx; }).slice(0,3).map(function(item) {
+        behaviors.append("<div class=\"output-summary--behavior output-summary--behavior_POSITIVE\"><i class=\"icon icon-likely\"></i>" + item.name + "</div>\n");
       });
       behaviors.append("</div>");
       behaviors.append("<h4 class=\"base--h4\">You are unlikely to______ </h4>");
       behaviors.append("<div class=\"output-summary--unlikely-behaviors\">");
-      unlikelycps.slice(0,3).map(function(item) {
-        behaviors.append("<div class=\"output-summary--behavior output-summary--behavior_NEGATIVE\"><i class=\"icon icon-not-likely\"></i>" + item + "</div>\n");
+      unlikelycps.sort(function(l,r) { return l.idx > r.idx; }).slice(0,3).map(function(item) {
+        behaviors.append("<div class=\"output-summary--behavior output-summary--behavior_NEGATIVE\"><i class=\"icon icon-not-likely\"></i>" + item.name + "</div>\n");
       });
       behaviors.append("</div>");
       cpsect.show();
