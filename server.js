ba04696e5dd68a5e968ba3473ba6c571951a3083
German Attanasio <germanattanasio@gmail.com>
2018-06-22 17:04:38 -0400

    chore(npm): Update dependencies, tests, README

--- server.js
+++ server.js
@@ -2,9 +2,6 @@
 
 require('dotenv').config({ silent: true });
 
-// Deployment tracking
-require('cf-deployment-tracker-client').track();
-
 const server = require('./app');
 const port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 
